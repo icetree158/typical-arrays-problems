31f6754b80d32d65b33e66e947cda6ccb89d5ac1
icetree158 <79416180+icetree158@users.noreply.github.com>
2021-03-15 17:18:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arr === undefined||arr.length===0){
+        return 0;
+    }
+    let minn=arr[0];
+    for(let i=0; i<arr.length;i++){
+        if(arr[i]<minn){
+            minn=arr[i];
+        }
+    }
+    return minn;
 }
 
 exports.max = function max (array) {
-  return 0;
+ if(arr === undefined||arr.length===0){
+        return 0;
+    }
+    let maxn=arr[0];
+    for(let i=0; i<arr.length;i++){
+        if(arr[i]>maxn){
+            maxn=arr[i];
+        }
+    }
+    return maxn;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   if(arr === undefined||arr.length===0){
+        return 0;
+    }
+    let sumn=0;
+    for(let i=0; i<arr.length;i++){
+        sumn+=arr[i];
+        }
+
+    return sumn/(arr.length);
 }
